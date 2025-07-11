6ee595cf4adef499cedc6dd44aa66e37ef84a838
cornenkiV <vladimircornenki02@gmail.com>
2025-07-11 13:18:56 +0200

    feat: Initial spring boot setup

---
+++ backend/src/main/java/com/webads/web_ads_backend/WebAdsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.webads.web_ads_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAdsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAdsBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/webads/web_ads_backend/WebAdsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webads.web_ads_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAdsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
