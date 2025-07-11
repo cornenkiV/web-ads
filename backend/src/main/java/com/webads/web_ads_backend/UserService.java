d155d787b63ba2fc0eabab7c72aba3191981cb8d
cornenkiV <vladimircornenki02@gmail.com>
2025-07-11 14:06:25 +0200

    feat: user service

---
+++ backend/src/main/java/com/webads/web_ads_backend/UserService.java
@@ -0,0 +1,24 @@
+package com.webads.web_ads_backend;
+
+import com.webads.web_ads_backend.model.User;
+import com.webads.web_ads_backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    /**
+     * Finds user by ID
+     *
+     * @param id user id
+     * @return User object
+     * @throws RuntimeException if user doesnt exist
+     */
+    public User getUser(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User with id: " + id + " not found."));
+    }
+}
